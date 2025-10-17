034b9381665e85ef569562e9eaca528a6b58e6f5
Ivan Gorokhov <xgorokhovx@gmail.com>
2025-10-17 15:50:22 -0400

    bugfix: actual Hb  can not be higher than target Hb

--- app/schemas/calculationForm.schema.ts
+++ app/schemas/calculationForm.schema.ts
@@ -3,5 +3,16 @@ import * as yup from "yup";
 export const calculationFormSchema = yup.object({
   weight: yup.string().required("Weight is required"),
   targetHb: yup.string().required("Target Hb is required"),
-  actualHb: yup.string().required("Actual Hb is required"),
+  actualHb: yup.string().required("Actual Hb is required").test(
+      "less-than-target",
+      "Actual Hb must be less than Target Hb",
+      function (value) {
+        const { targetHb } = this.parent;
+
+        const targetHbNumber = Number(targetHb)
+        const actualHbNuumber = Number(value)
+        if (actualHbNuumber == null || targetHbNumber == null) return true;
+        return actualHbNuumber < targetHbNumber;
+      }
+  ),
 });
